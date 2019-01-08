375349cc9920c5250a4ccf3740fc3ede78b7c981
yyj <1453054794@qq.com>
2019-01-08 18:17:58 +0800

    提交图片路由

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,24 @@ client.url("127.0.0.1:8080");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post("/upload", async function (req, res) {
+  let form = new multiparty.Form({
+    uploadDir: "./public/upload"//手动在public下建一个upload文件，图片上传会存到这个文件
+  });
+  form.parse(req, function (err, fields, files) {
+    console.log(files); //{[{}]}的形式
+    let key = Object.keys(files)[0];
+    if (err) {
+      res.send(err);
+    } else {
+      // res.send(path.basename(files.uploadHeader[0].path));//path是文件名
+      res.send(path.basename(files[key][0].path))//这种方法解决上传时不同的name
+    }
+  })
+})
 module.exports = router;
