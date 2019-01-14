ca2b93c7aee16e026719bd2e7946d311cca25b84
Tangyupeng <362690293@qq.com>
2019-01-15 02:34:06 +0800

    1

--- routes/storegoodtongji.js
+++ routes/storegoodtongji.js
@@ -8,49 +8,22 @@ client.url("127.0.0.1:8080");
 //查找折线图数组的servename
 router.get("/", async function (req, res) {
     let { storeId, year } = req.query;
-    let data = await client.get("/serve", {
+    console.log("111111",storeId)
+    let data = await client.get("/storegoods", {
         "stores.$id": storeId,
         submitType: "findJoin", ref: "stores"
     })
+    console.log("????",data);
     let servename = [];
     for (let i = 0; i < data.length; i++) {
-        servename.push(data[i].severname)
+        servename.push(data[i].name)
     }
-
     //查找折线图中的series,
     let data2 = await client.get("/orderbuy", {
         "stores.$id": storeId,
         submitType: "findJoin", ref: "stores"
     })
     let series = [];
-    // for (let i = 0; i < data2.length; i++) {
-    //     //已经判断了年
-    //     if (data2[i].buytime.split(" ")[0].split("-")[0] == year) {
-    //         //分辨月
-    //         for (let j = 0; j < servename.length; j++) {
-    //             for (let ij = 0; ij < data2[i].orderservearr.length; ij++) {
-    //                 if (servename[j] == data2[i].orderservearr[ij].shangpingname) {
-    //                     let obj = {};
-    //                     obj.name = servename[j];
-    //                     obj.type = "line";
-    //                     // obj.stack = "总量"
-    //                     obj.data = [];
-    //                     for (let month = 1; month <= 12; month++) {
-    //                         if (data2[i].buytime.split(" ")[0].split("-")[1] == month) {
-    //                             obj.data.push(data2[i].orderservearr[ij].num)
-    //                         } else {
-    //                             obj.data.push(0)
-    //                         }
-    //                     }
-    //                     series.push(obj)
-    //                 }
-    //             }
-    //         }
-
-    //     }
-    // }
-
-
     for (let j = 0; j < servename.length; j++) {
         // console.log(servename[j])
         let obj = {};
@@ -64,9 +37,9 @@ router.get("/", async function (req, res) {
             for (let i = 0; i < data2.length; i++) {
                 if (data2[i].buytime.split(" ")[0].split("-")[0] == year &&
                     data2[i].buytime.split(" ")[0].split("-")[1] == month) {
-                    for (let ij = 0; ij < data2[i].orderservearr.length; ij++) {
-                        if (servename[j] == data2[i].orderservearr[ij].shangpingname) {
-                            num +=data2[i].orderservearr[ij].num
+                    for (let ij = 0; ij < data2[i].ordergoodarr.length; ij++) {
+                        if (servename[j] == data2[i].ordergoodarr[ij].shangpingname) {
+                            num +=data2[i].ordergoodarr[ij].num
                         }
                     }
                 }
@@ -91,7 +64,7 @@ router.get("/", async function (req, res) {
 //查找折线图数组的servename
 router.get("/years", async function (req, res) {
     let { storeId, year } = req.query;
-    let dataname = await client.get("/serve", {
+    let dataname = await client.get("/storegoods", {
         "stores.$id": storeId,
         submitType: "findJoin", ref: "stores"
     })
@@ -105,14 +78,14 @@ router.get("/years", async function (req, res) {
     let dataarr = [];
     for (let i = 0; i < dataname.length; i++) {
         let obj = {}
-        obj.name = dataname[i].severname;
+        obj.name = dataname[i].name;
         let value = 0;
         for (let j = 0; j < data2num.length; j++) {
             if (data2num[j].buytime.split(" ")[0].split("-")[0] == year) {
-                for (let ij = 0; ij < data2num[j].orderservearr.length; ij++) {
-                    if (dataname[i].severname == data2num[j].orderservearr[ij].shangpingname) {
-                        // console.log(data2num[j].orderservearr[ij].num)
-                        value += data2num[j].orderservearr[ij].num
+                for (let ij = 0; ij < data2num[j].ordergoodarr.length; ij++) {
+                    if (dataname[i].name == data2num[j].ordergoodarr[ij].shangpingname) {
+                        // console.log(data2num[j].ordergoodarr[ij].num)
+                        value += data2num[j].ordergoodarr[ij].num
                     }
                 }
 
@@ -129,14 +102,13 @@ router.get("/years", async function (req, res) {
 //柱状图
 router.get("/jidu", async function (req, res) {
     let { storeId, year } = req.query;
-    let data = await client.get("/serve", {
+    let data = await client.get("/storegoods", {
         "stores.$id": storeId,
         submitType: "findJoin", ref: "stores"
     })
-    console.log(data)
     let servename = [];
     for (let i = 0; i < data.length; i++) {
-        servename.push(data[i].severname)
+        servename.push(data[i].name)
     }
 
     //查找折线图中的series,
@@ -163,9 +135,9 @@ router.get("/jidu", async function (req, res) {
             for (let i = 0; i < data2.length; i++) {
                 if (data2[i].buytime.split(" ")[0].split("-")[0] == year &&
                     data2[i].buytime.split(" ")[0].split("-")[1] == month) {
-                    for (let ij = 0; ij < data2[i].orderservearr.length; ij++) {
-                        if (servename[j] == data2[i].orderservearr[ij].shangpingname) {
-                            num +=data2[i].orderservearr[ij].num
+                    for (let ij = 0; ij < data2[i].ordergoodarr.length; ij++) {
+                        if (servename[j] == data2[i].ordergoodarr[ij].shangpingname) {
+                            num +=data2[i].ordergoodarr[ij].num
                         }
                     }
                 }
