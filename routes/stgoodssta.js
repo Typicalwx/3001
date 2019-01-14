81817e04eba60d2aa54993ec36af59a462302b79
yyj <1453054794@qq.com>
2019-01-14 16:22:04 +0800

    提交

--- routes/stgoodssta.js
+++ routes/stgoodssta.js
@@ -11,28 +11,41 @@ client.url("127.0.0.1:8080");
 
 router.get("/salesvolume", async function (req, res) {
     let { storeId } = req.query;
-    console.log(id)
+    console.log(storeId)
     let data = await client.get("/orderbuied", {
         "stores.$id": storeId,
         submitType: "findJoin", ref: "stores",
     })
-    console.log(data)
+    // console.log(data)
     let sales = []
     let k = 0
-    for (let i = 0; i < data.length; i++) {
-        let ordergoodarr = data[i].ordergoodarr
-        for (let j = 0; j < ordergoodarr.length; j++) {
-            for (k = 0; k < sales.length; k++) {
-                if (ordergoodarr[j].shangpingid == sales[k].shangpingid) {
-                    sales[k].total += ordergoodarr[j].total;
-                    break;
+    var myDate = new Date()
+    for (let i = 0; i < 6; i++) {
+        let date = myDate.toLocaleDateString()
+        let nowDate = date.split("-")
+        if (nowDate[1] - i <= 0) {
+            nowDate[1] = 12 - i + 1;
+            nowDate[0] = nowDate[0] - 1
+        }
+        let obj = {}
+        obj.date = nowDate[0] + "." + nowDate[1];
+        obj.total = 0
+        sales.push(obj)
+        for (let m = 0; m < data.length; m++) {
+            let ordergoodarr = data[m].ordergoodarr
+            let dtArr = data[m].buytime.split(" ");
+            console.log(dtArr, "结构前")
+            dtArr = dtArr[0].split("-");
+            console.log(dtArr, "结构后")
+            if (nowDate[0] == dtArr[0] && dtArr[1] == nowDate[1]) {
+                for (let j = 0; j < ordergoodarr.length; j++) {
+                    sales[i].total += ordergoodarr[j].total;
                 }
-            }
-            if (k == sales.length - 1) {
-                sales.push(ordergoodarr[j]);
+                data.splice(m, 1);
+                console.log(data.length)
+                m--;
             }
         }
-
     }
     res.send(sales);
 });
